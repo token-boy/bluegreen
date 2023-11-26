6d09a83cb1bd4b0caf66c7fc20ca949b85efd940
Mxsyx <mxsyxin@gmail.com>
2023-11-26 20:27:53 +0800

    ⭐️ 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const port = 80
-const version = 0
+const version = 1
 
 app.get('/', (req, res) => {
   res.send(version.toString())
--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
+t=$(date +%s)
 while true
 do
   curl https://bluegreen.mxsyx.site
+  tt=$(date +%s)
+  t=$tt
+  echo -e '\r'
+  echo `expr $tt - $t`
   echo -e "\n"
 done
